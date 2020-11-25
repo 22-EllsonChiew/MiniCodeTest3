d744aa15f0e28e65fb17cb6b4ee24a1a492bb90a
Itzuzume <72852531+Itzuzume@users.noreply.github.com>
2020-11-25 17:10:05 +0800

    MiniTest3_233

---
+++ CodeMiniTest_3/Assets/Scripts/PlaneMoving.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneMoving : MonoBehaviour
+{
+    bool isMovingforward = true;
+    bool isMovingBack = false;
+    float zLowerLimit = 24.37f;
+    float moveSpeed = 2.0f;
+    float zUpperLimit = 31.76f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(isMovingBack && !isMovingforward)
+        {
+            if(transform.position.z >= zLowerLimit)
+            {
+                transform.Translate(Vector3.back * Time.deltaTime * moveSpeed);
+            }
+            else
+            {
+                isMovingBack = !isMovingBack;
+                isMovingforward = !isMovingforward;
+            }
+        }
+
+        if(isMovingforward && !isMovingBack)
+        {
+            if(transform.position.z <= zUpperLimit)
+            {
+                transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+            }
+            else
+            {
+                isMovingBack = !isMovingBack;
+                isMovingforward = !isMovingforward;
+            }
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.parent = transform;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.parent = null;
+        }
+    }
+
+
+
+
+}
--- CodeMiniTest_3/Assets/Scripts/PlayerController.cs
+++ CodeMiniTest_3/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 public class PlayerController : MonoBehaviour
 {
     public Animator AniPlayer;
-    
+
     public float speed;
     bool IsMove = false;
-
-
-
-
+    public Text PUscore;
+    float icount = 0.0f;
+    public Animator bridgeT;
+    float iTotalPowerUpLeft = 4.0f;
+    bool BridgeTurn = false;
+    public Text Timer;
+    float timercount = 10.0f;
+    float CountDown = 0.0f;
 
     Rigidbody PlayerRb;
 
@@ -29,37 +36,108 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+
+
+
+
         float verticalinput = Input.GetAxis("Vertical");
         float horizontalinput = Input.GetAxis("Horizontal");
 
 
 
+
+
+
+
+
         if (Input.GetKey(KeyCode.W))
         {
-            transform.rotation= Quaternion.Euler (0, 0, 0);
+            transform.rotation = Quaternion.Euler(0, 0, 0);
             AniPlayer.SetBool("IsMove", true);
             StartRun();
-            
+
         }
-        if(Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S))
         {
             transform.rotation = Quaternion.Euler(0, 180, 0);
             AniPlayer.SetBool("IsMove", true);
             StartRun();
-            
+
         }
-        else if(Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.W))
+        else if (Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.W))
         {
             AniPlayer.SetBool("IsMove", false);
         }
-        
-    }
-    void OnCollisionEnter(Collision collision)
-    {
-        if(collision.gameObject.CompareTag("Cone"))
+
+        if (Input.GetKey(KeyCode.A))
         {
-            Debug.Log("Bridge");
+            transform.rotation = Quaternion.Euler(0, -90, 0);
+            AniPlayer.SetBool("IsMove", true);
+            StartRun();
+
         }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.rotation = Quaternion.Euler(0, 90, 0);
+            AniPlayer.SetBool("IsMove", true);
+            StartRun();
+
+        }
+        else if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+        {
+            AniPlayer.SetBool("IsMove", false);
+        }
+
+        if (BridgeTurn == true && timercount >= 0)
+        {
+            Timer.GetComponent<Text>().text = "Time: " + timercount.ToString("0");
+            timercount -= Time.deltaTime;
+        }
+        else
+        {
+            Timer.GetComponent<Text>().text = "==" + "==";
+            bridgeT.SetBool("ConeTrue", false);
+        }
+
+    }
+
+
+       
+    
+    
+
+    private void OnTriggerEnter(Collider col)
+    {
+        
+        if (iTotalPowerUpLeft == 0)
+        {
+            if (col.gameObject.CompareTag("Cone"))
+            {
+                bridgeT.SetBool("ConeTrue", true);
+                BridgeTurn = true;
+
+            }
+            
+        }
+        else if(iTotalPowerUpLeft < 1 )
+        {
+            if (col.gameObject.CompareTag("Cone"))
+            {
+                Debug.Log("Collect all power up");
+
+            }
+            
+        }
+        if (col.gameObject.CompareTag("Coins"))
+        {
+            iTotalPowerUpLeft--;
+            icount++;
+            PUscore.GetComponent<Text>().text = "Coin Collected: " + icount.ToString();
+
+            Destroy(col.gameObject);
+        }
+
     }
 
     void StartRun()
@@ -69,5 +147,4 @@ public class PlayerController : MonoBehaviour
     }
 
     
-
 }
